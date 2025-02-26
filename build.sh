18c15c028ea77b8f5fb5c2bafccc79a1cc08b1ff
Fabien Karsegard <fabien@karsegard.ch>
2025-02-26 16:35:44 +0100

    Update Dockerfile, Dockerfile, and 10 more files...

--- build.sh
+++ build.sh
@@ -6,9 +6,11 @@ eval $(docker-machine env -u)
 
 for version in "$@"
 do
+    echo building $version
     pushd $version
-    docker build -t registry.gitlab.com/karsegard/docker-infomaniak:$version .
-    docker push registry.gitlab.com/karsegard/docker-infomaniak:$version
+    #docker build -t registry.gitlab.com/karsegard/docker-infomaniak:$version .
+    docker buildx build -t registry.gitlab.com/karsegard/docker-infomaniak:$version .
+    #docker push registry.gitlab.com/karsegard/docker-infomaniak:$version
     popd
 done
 
